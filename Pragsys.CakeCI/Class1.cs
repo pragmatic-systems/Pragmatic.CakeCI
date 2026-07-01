911a1b86df859fba19f16042358784acd9de089f
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-07-01 17:56:01 +0100

    Add dotnet 10 project.

---
+++ Pragsys.CakeCI/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+
+namespace Pragsys.CakeCI;
+
+[CakeAliasCategory("SonarQube")]
+public static class SonarResults
+{
+    [CakeMethodAlias]
+    [CakeAliasCategory("Scan Check")]
+    public static async Task SonarScanCheck(this ICakeContext context)
+    {
+        ICakeLog logger = context.Log;
+        logger.Information("=========================================");
+        logger.Information("SONAR SCAN CHECK SHIP");
+        logger.Information("=========================================");
+    }
+}
