7186c2013505f02ae0113507a812b261dd4890f9
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-07-03 16:10:06 +0100

    Add linting check

--- Pragsys.CakeCI/CiArgumentAliases.cs
+++ Pragsys.CakeCI/CiArgumentAliases.cs
@@ -11,12 +11,10 @@ public static class CiArgumentAliases
 {
     [CakeMethodAlias]
     [CakeAliasCategory("Arguments")]
-    public static string CiArgument(this ICakeContext context, string argumentName)
+    public static string? CiArgument(this ICakeContext context, string argumentName)
     {
-        string? result = null;
-
         // Normal CMD Argument First
-        result = context.Arguments.GetArgument(argumentName);
+        var result = context.Arguments.GetArgument(argumentName);
         if (!string.IsNullOrEmpty(result))
             return result;
 
@@ -57,5 +55,27 @@ public static class CiArgumentAliases
             return result;
 
         return defaultValue;
+    }
+    [CakeMethodAlias]
+    [CakeAliasCategory("Lint")]
+    public static void CiLint(this ICakeContext context)
+    {
+        context.Log.Information("Running lint check with dotnet format...");
+
+        // Run `dotnet format --verify-no-changes`
+        var settings = new ProcessSettings();
+        settings.WithArguments(a =>
+        {
+            a.Append("format");
+            a.Append("--verify-no-changes");
+        });
+
+        using var result = context.ProcessRunner.Start("dotnet", settings);
+        result.WaitForExit();
+        if (result.GetExitCode() != 0)
+        {
+            throw new CakeException("Lint check failed: code formatting violations detected. Run `dotnet format`");
+        }
+        context.Log.Information("Lint check passed – no formatting changes required.");
     }
 }
