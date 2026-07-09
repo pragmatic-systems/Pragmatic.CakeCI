e8902c24bf2593efa8d68bcd9e5fa7a4f0a5102d
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-07-09 08:11:58 +0100

    Args tests

---
+++ Pragsys.CakeCI.Tests/NugetArgsTests.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Pragsys.CakeCI.Tests;
+
+public class NugetArgsTests
+{
+    [Fact]
+    public void NugetArgs_ShouldValidate()
+    {
+        var args = new NugetArgs
+        {
+            Source = string.Empty,
+            ApiKey = string.Empty,
+        };
+
+        Should
+            .Throw<ArgumentException>(() => args.Validate())
+            .Message.ShouldBe("NugetPackageSource (Source) is required.");
+
+        args.Source = "https://api.nuget.org/v3/index.json";
+        Should
+            .Throw<ArgumentException>(() => args.Validate())
+            .Message.ShouldBe("NugetApiKey (ApiKey) is required.");
+
+        args.ApiKey = "my-api-key";
+        args.Validate();
+    }
+}
