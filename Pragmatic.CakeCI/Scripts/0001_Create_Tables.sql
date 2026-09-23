b7eaf1838c2e41ddc129ee66695db8b59c59cf58
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-09-23 14:53:37 +0100

    Remove scripts file - We have proven the mechanism.

--- Pragmatic.CakeCI/Scripts/0001_Create_Tables.sql
+++
@@ -1,8 +0,0 @@
--- Dogfood fixture: exercises the Oracle PL/SQL analysis path for .sql files.
--- The VARCHAR column intentionally triggers plsql:S1192 ("Use VARCHAR2 instead of VARCHAR").
--- We can exclude this in the build.cake via sonar.exclusions (SonarArgs.AdditionalProperties)
--- to verify the extra-properties passthrough end-to-end in the Sonar scan.
-CREATE TABLE Dogfood_Plsql_Scan (
-    Id INT,
-    Name VARCHAR(100)
-);
